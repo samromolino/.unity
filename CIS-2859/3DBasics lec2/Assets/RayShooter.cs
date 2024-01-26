b50c5fb9f2a3b2aa12533f854963bafc03b3e16a
samromolino <samromolino@gmail.com>
2024-01-26 18:29:13 -0500

    gun shoot

---
+++ CIS-2859/3DBasics lec2/Assets/RayShooter.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+
+    private Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // 0 is lmb
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(camera.pixelWidth / 2, camera.pixelHeight / 2, 0);
+            Ray ray = camera.ScreenPointToRay(point);
+            RaycastHit hit;
+
+            /*out keyword from C# allows us to pass the variable by reference, when the
+                variable hasn't necessarily been initialized yet.
+                In other words, the out ensures that the method it's being passed to must give that
+                parameter a value (otherwise, it will be a compiler error)
+            */
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 position)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = position;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = camera.pixelWidth / 2 - size / 4;
+        float posY = camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+}
