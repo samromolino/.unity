8bd3cfd7ff94d974feb201747b1c8b5215bda522
samromolino <samromolino@gmail.com>
2024-01-27 15:25:25 -0500

    enemy move and shoot

---
+++ CIS-2859/3DBasics lec2/Assets/Enemy Scripts/WanderingAI.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 3.0f;
+
+    [SerializeField]
+    private float obstacleRange = 5.0f;
+
+    [SerializeField]
+    private GameObject fireballPrefab;
+
+    private GameObject fireball;
+
+    private bool isAlive;
+
+    void Start()
+    {
+        isAlive = true;
+    }
+
+    public void SetAlive(bool isAlive)
+    {
+        this.isAlive = isAlive;
+    }
+
+    void Update()
+    {
+        if (isAlive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if(fireball == null)
+                    {
+                        fireball = Instantiate(fireballPrefab) as GameObject;
+
+                        fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        fireball.transform.rotation = transform.rotation;
+                    }
+                }
+
+                else if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+
+    }
+}
---
+++ CIS-2859/3DBasics lec2/Assets/Scenes/SceneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    private GameObject enemy;
+
+    void Update()
+    {
+        if (enemy == null)
+        {
+            enemy = Instantiate(enemyPrefab) as GameObject;
+            enemy.transform.position = new Vector3(0, 1, 1);
+            float angle = Random.Range(0, 360);
+            enemy.transform.Rotate(0, angle, 0);
+        }
+
+    }
+}
